853fb14752d2948f5dd390bbc8815671b21cf078
liaowuxukong <liaowuxukong@163.com>
2013-09-03 18:18:57 +0800

    18

--- bin/boot.py
+++ bin/boot.py
@@ -7,14 +7,4 @@ import SocketServer
 import os
 import sys
 
-if len(sys.argv) == 2:
-    print 'Serving files from [%s]' % sys.argv[1]
-    os.chdir(sys.argv[1])
-else:
-    print 'Service files from [%s]' % os.getcwd()
-
-httpd = SocketServer.TCPServer(
-    (os.environ.get('VCAP_APP_HOST', 'localhost'),
-     int(os.environ.get('VCAP_APP_PORT', 8080))),
-    SimpleHTTPServer.SimpleHTTPRequestHandler)
-httpd.serve_forever()
+print "hello"
