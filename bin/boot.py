00cd9e01e310233f05e323e48e8376fb617906e4
liaowuxukong <liaowuxukong@163.com>
2013-09-03 18:42:20 +0800

    18

--- bin/boot.py
+++ bin/boot.py
@@ -7,6 +7,12 @@ import SocketServer
 import os
 import sys
 
+port = int(os.environ.get('VCAP_APP_PORT', 8080)
+if len(sys.argv) == 2:
+    print 'Serving files from [%s]' % sys.argv[1]
+    os.chdir(sys.argv[1])
+else:
+    print 'Service files from [%s]' % os.getcwd()
 
 httpd = SocketServer.TCPServer(
     (os.environ.get('VCAP_APP_HOST', 'localhost'),
