1bd4c2df0d04f16ce271f8369ee478ed3c00e511
liaowuxukong <liaowuxukong@163.com>
2013-09-03 18:47:14 +0800

    18

--- bin/boot.py
+++ bin/boot.py
@@ -17,6 +17,6 @@ else:
 
 httpd = SocketServer.TCPServer(
     (os.environ.get('VCAP_APP_HOST', 'localhost'),
-     int(os.environ.get('VCAP_APP_PORT', 8080))),
+     int(os.environ.get('VCAP_APP_PORT', 8888))),
     SimpleHTTPServer.SimpleHTTPRequestHandler)
 httpd.serve_forever()
